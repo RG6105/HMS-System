0e91bedbc1e27da202f6e353326be870d3f52363
RG6105 <artheon.code@gmail.com>
2026-01-08 20:44:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from applications.models import db, doctors, patients, appointment, treatment, availability
+import os
+
+app=Flask(__name__)
+app.secret_key="P@ssword"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath(os.getcwd()) + '/database/database.sqlite3'
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+from applications.controllers import*
+if __name__ == '__main__':
+    app.run(debug=True)
